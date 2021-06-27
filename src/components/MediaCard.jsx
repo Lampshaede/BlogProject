b31a83ea0a29ceda259b0af3437140f457eab48a
Luke Lampman <lukeb.lampman@gmail.com>
2021-06-27 10:16:04 -0500

    fix errors

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -3,7 +3,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Link, Typography } from '@material-ui/core';
 import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   root: {
